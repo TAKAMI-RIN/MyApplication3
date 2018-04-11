545e02b4ad12d1298874b94087cef690ffb884ec
TAKAMI-RIN <ivkq65648@163.com>
2018-04-11 19:13:01 +0800

    TOOSAKA

--- app/src/main/java/com/example/tohsaka/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/tohsaka/myapplication3/MainActivity.java
@@ -4,25 +4,17 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.app.Activity;
-import android.support.v7.widget.ViewUtils;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Spinner;
 
-import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
 //    private Button Button2;
 //    private Button RButton;
     private SharedPreferences prefs;
+    public char ww = 1;
     //int number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +27,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onclickMethod2(View v) {
-        EditText edtA = (EditText) findViewById(R.id.editText);
-        EditText edtB = (EditText) findViewById(R.id.editText2);
-        Spinner opr = (Spinner) findViewById(R.id.spinner);
-        TextView tv = (TextView) findViewById(R.id.textView3);
+        //EditText edtA = (EditText) findViewById(R.id.editText);
+        EditText edtA =  findViewById(R.id.editText);
+        EditText edtB =  findViewById(R.id.editText2);
+        Spinner opr =  findViewById(R.id.spinner);
+        TextView tv =  findViewById(R.id.textView3);
         String strA = edtA.getText().toString();
         String strB = edtB.getText().toString();
         double a = strA.trim().equalsIgnoreCase("") ? 0 : Double.parseDouble(strA);
@@ -73,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         // 取得Editor物件
         SharedPreferences.Editor prefEdit = prefs.edit();
         // 存入偏好設定資料至Editor物件
-      //  prefEdit.putString(PREF_AMOUNT, txtAmount.getText().toString());
+        //prefEdit.putString(ww);
       //  float rate;
       //  rate = (float) Double.parseDouble(txtRate.getText().toString());
         //.putFloat(PREF_RATE, rate);
@@ -81,9 +74,9 @@ public class MainActivity extends AppCompatActivity {
     }
     //public void onclickMethod(View v){
         //TextView tv = (TextView) findViewById(R.id.textView7);
-        //Random r = new Random();
-        //number = r.nextInt(10) + 1;
-        //tv.setText("REDO");
+    //        //Random r = new Random();
+    //        //number = r.nextInt(10) + 1;
+    //        //tv.setText("REDO");
     //}
 }
 
